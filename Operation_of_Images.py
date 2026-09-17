8194318f61ca0522b6e8f6eebcd9ac025d7d8dec
phantom2318 <25dcs095@charusat.edu.in>
2026-09-17 22:26:09 +0530

    Resizing operation

--- Operation_of_Images.py
+++ Operation_of_Images.py
@@ -1,35 +1,42 @@
 import numpy as np
-from PIL import Image
 import cv2 as cv
+from tkinter import Tk,filedialog
 import matplotlib.pyplot as plt
-from tkinter import filedialog,Tk
-
 #Select an image from Device
 def SelectImage()->str:
-  root = Tk()
-  root.withdraw()
-  file_path = filedialog.askopenfilename(
+ root=Tk()
+ root.withdraw()
+ file_path = filedialog.askopenfilename(
     title="Select an image",
     filetypes=[
-      ("Image files", "*.jpg *.jpeg *.png *.bmp *.tiff"),
-      ("All files", "*.*")
+        ("Image files", "*.jpg *.jpeg *.png *.bmp *.tiff"),
+        ("All files", "*.*")
     ]
-  )
-  return file_path
+    )
+ #if image is not selected by the user
+ if not file_path:
+    print("No image selected.")
+    exit()
+ return file_path
 
+#plotting an image
 def Subplot(image1=None,image2=None,cmap1=None,cmap2=None,title1="Image-1",title2="Image-2"):
 
    if image1 is None:
     f_image1=SelectImage()
     image1=cv.imread(f"{f_image1}")
+
     if image1 is None:
        raise FileNotFoundError(f"Could not read image: {f_image1}")
+    
     image1=cv.cvtColor(image1,cv.COLOR_BGR2RGBA)
    if image2 is None:
      f_image2=SelectImage()
      image2=cv.imread(f"{f_image2}")
+
      if image2 is None:
        raise FileNotFoundError(f"Could not read image: {f_image2}")
+     
      image2=cv.cvtColor(image2,cv.COLOR_BGR2RGBA)
 
    plt.subplot(1,2,1)
@@ -42,12 +49,18 @@ def Subplot(image1=None,image2=None,cmap1=None,cmap2=None,title1="Image-1",title
 
    plt.show()
 
+path=SelectImage()
+image=cv.imread(path)
 
-image=np.array([])
-print(image.shape)
-z=np.array([[input("Enter the RGB value")]])
-image=np.full((300,400),255)
-print(image.shape)
-print(image)
+if image is None:
+   raise FileNotFoundError(f"Could not read image: {path}")
+(h,w)=image.shape[0:2]
+aspect=w/h
 
-Subplot(image,None)
+##after resizing height to it's double
+h=int(h*2)
+w=int((aspect*h))
+dimension=(w,h)
+
+r_image=cv.resize(image,dimension,interpolation=cv.INTER_AREA)
+Subplot(image,r_image)
