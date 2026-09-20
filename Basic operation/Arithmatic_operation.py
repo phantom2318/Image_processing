45f4119b68f4f7a354b47825314cccb18888ae2d
phantom2318 <25dcs095@charusat.edu.in>
2026-09-20 22:09:35 +0530

    Binary operations included

--- Basic operation/Arithmatic_operation.py
+++ Basic operation/Arithmatic_operation.py
@@ -27,6 +27,7 @@ def Substraction(img1: np.ndarray,img2: np.ndarray):
   substraction21=cv.cvtColor(substraction21,cv.COLOR_BGR2RGB)
 
   ipr.subplot(substraction12,substraction21,title1="img`1`-img`2`",title2="img`2`-img`1`")
+
 path1=ipr.select_image()
 path2=ipr.select_image()
 
@@ -44,3 +45,30 @@ img2 = cv.resize(img2, (1000, 1000), interpolation=cv.INTER_AREA)
 simpleAddition(img1,img2)
 WeightedAddition(img1,img2)
 Substraction(img1,img2)
+img_and_12=cv.bitwise_and(img1,img2)
+img_or_12=cv.bitwise_or(img1,img2)
+img_xor_12=cv.bitwise_xor(img1,img2)
+img_not_1=cv.bitwise_not(img1)
+
+img_and_12=cv.cvtColor(img_and_12,cv.COLOR_BGR2RGB)
+img_or_12=cv.cvtColor(img_or_12,cv.COLOR_BGR2RGB)
+img_xor_12=cv.cvtColor(img_xor_12,cv.COLOR_BGR2RGB)
+img_not_1=cv.cvtColor(img_not_1,cv.COLOR_BGR2RGB)
+
+plt.subplot(2,2,1)
+plt.imshow(img_and_12)
+plt.title("AND operation")
+
+plt.subplot(2,2,3)
+plt.imshow(img_or_12)
+plt.title("OR operation")
+
+plt.subplot(2,2,2)
+plt.imshow(img_xor_12)
+plt.title("XOR operation")
+
+plt.subplot(2,2,4)
+plt.imshow(img_not_1)
+plt.title("NOT operation")
+
+plt.show()
